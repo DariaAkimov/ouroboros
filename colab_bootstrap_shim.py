74fa8e02b48cc69ff307ed15b1419ed98e4a4a41
Darya_A <daryadariaa229@gmail.com>
2026-06-03 09:58:13 +0300

    change directiry to ouroboros_dev

--- colab_bootstrap_shim.py
+++ colab_bootstrap_shim.py
@@ -55,7 +55,7 @@ assert GITHUB_USER, "GITHUB_USER not set. Add it to your config cell (see README
 assert GITHUB_REPO, "GITHUB_REPO not set. Add it to your config cell (see README)."
 BOOT_BRANCH = str(os.environ.get("OUROBOROS_BOOT_BRANCH", "ouroboros"))
 
-REPO_DIR = pathlib.Path("/content/ouroboros_repo").resolve()
+REPO_DIR = pathlib.Path("/content/drive/MyDrive/ouroboros_dev").resolve()
 REMOTE_URL = f"https://{GITHUB_TOKEN}:x-oauth-basic@github.com/{GITHUB_USER}/{GITHUB_REPO}.git"
 
 if not (REPO_DIR / ".git").exists():
